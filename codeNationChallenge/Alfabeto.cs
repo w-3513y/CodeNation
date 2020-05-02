e600354b6af48753f7f9e5851954051f02b59d8e
w-31l3y <wesleyferreirasb@gmail.com>
2020-05-01 22:25:40 -0300

    first commit

---
+++ codeNationChallenge/Alfabeto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace codeNationChallenge
+{
+    public static class Alfabeto
+    {
+        public static char[] Letra = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+    }
+}
---
+++ codeNationChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace codeNationChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var requisicaoWeb = WebRequest.CreateHttp("https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=getToken");
+            requisicaoWeb.Method = "GET";            
+            requisicaoWeb.UserAgent = "RequisicaoWebDemo";
+
+            using (var resposta = requisicaoWeb.GetResponse())
+            {
+                var streamDados = resposta.GetResponseStream();
+                StreamReader reader = new StreamReader(streamDados);
+                object objResponse = reader.ReadToEnd();
+                var post = JsonConvert.DeserializeObject<Post>(objResponse.ToString());
+                Console.WriteLine("número de casas:      " + post.numero_casas + "\n" +
+                                  "token:                " + post.Token + "\n" +
+                                  "cifrado:              " + post.Cifrado + "\n" +
+                                  "decifrado:            " + post.decifrado + "\n" +
+                                  "resumo criptografico: " + post.resumo_criptografico);
+                Console.WriteLine();
+                Console.ReadLine();
+                streamDados.Close();
+                resposta.Close();
+            }
+            Console.ReadLine();
+        }
+
+    }
+
+}
